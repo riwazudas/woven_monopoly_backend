2b36af06683ab4991214c26eac6b66df37bca9e5
riwaz <riwazudas@gmail.com>
2026-03-22 17:04:59 +1100

    bug fix

--- config/initializers/board.rb
+++ config/initializers/board.rb
@@ -1,2 +1,4 @@
+require Rails.root.join("app/services/board_loader")
+
 Rails.application.config.x.monopoly ||= ActiveSupport::OrderedOptions.new
 Rails.application.config.x.monopoly.board = BoardLoader.load
--- config/initializers/game_config.rb
+++ config/initializers/game_config.rb
@@ -1,2 +1,4 @@
+require Rails.root.join("app/services/game_config")
+
 Rails.application.config.x.monopoly ||= ActiveSupport::OrderedOptions.new
 Rails.application.config.x.monopoly.game_config = GameConfig.defaults
